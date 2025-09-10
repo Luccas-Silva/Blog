990f7f943b14819d7ea68d6e02382c5d98c69749
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2025-09-09 23:47:10 -0300

    Customized color

--- components/SearchButton.tsx
+++ components/SearchButton.tsx
@@ -16,7 +16,7 @@ const SearchButton = () => {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          strokeWidth={1.5}
+          strokeWidth={2}
           stroke="currentColor"
           className="hover:text-primary-500 dark:hover:text-primary-400 h-6 w-6 text-gray-900 dark:text-gray-100"
         >
