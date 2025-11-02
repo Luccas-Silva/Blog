7a72e8d069a9f136c215a8bcb7a73b88ad91e814
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2025-11-01 23:38:15 -0300

    images

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -9,13 +9,13 @@ const projectsData: Project[] = [
   {
     title: 'GeoMonitor',
     description: `GeoMonitor é uma solução acadêmica criada no desafio Global Solution da FIAP para monitorar zonas de risco (encostas, barragens e áreas sujeitas a desastres). O projeto usa sensores IoT, IA e mensageria assíncrona para prever riscos, gerar alertas automáticos e agilizar respostas a eventos críticos.`,
-    imgSrc: '',
+    imgSrc: '/static/images/GEOMonitor.png',
     href: '/blog/GeoMonitor',
   },
   {
     title: 'Java BlackJack',
     description: `Java BlackJack é um projeto que recria o clássico jogo de cartas em Java, usando a Deck of Cards API, arquitetura em camadas e integração com banco de dados. Combina boas práticas de desenvolvimento com diversão para quem gosta de programação e jogos.`,
-    imgSrc: '',
+    imgSrc: '/static/images/Black-Jack.png',
     href: '/blog/Java_BlackJack',
   },
   {
@@ -27,7 +27,7 @@ const projectsData: Project[] = [
   {
     title: 'Chess System Java',
     description: `Chess System Java é um jogo de xadrez para terminal, desenvolvido em Java com orientação a objetos e regras oficiais, oferecendo uma experiência completa e educativa para jogadores e estudantes de programação.`,
-    imgSrc: '/static/images/Chess-System-Java.png',
+    imgSrc: '/static/images/Chess-Java.png',
     href: '/blog/Chess_System_Java',
   },
 ]
